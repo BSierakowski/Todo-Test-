2a2ee4c375960247fc2974535ebaf6c2171e0f3d
Brian Sierakowski <briansierakowski@gmail.com>
2011-06-10 09:12:24 -0400

    /todo should now return DB entries with a status of '1', uses template to format rows into a table

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from bottle import route, run
+import sqlite3
+from bottle import route, run, debug, template
 
-@route('/hello')
 @route('/todo')
-def hello():
-    return "Hello World!"
+def todo_list():
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
+    result = c.fetchall()
+    c.close()
+    output = template('make_table', rows=result)
+    return output
+    
+# debug(True)
+# run(reloader=True)        
+run()
 
-if __name__ == '__main__':
-  import bottle
-  app = bottle.app()
-  bottle.debug(True)
-  bottle.run(app=app,host='localhost', port=8080,reloader=True)
+
+
+# if __name__ == '__main__':
+#    import bottle
+#    app = bottle.app()
+#    bottle.debug(True)
+#    bottle.run(app=app,host='localhost', port=8080,reloader=True)
---
+++ make_table.tpl
@@ -0,0 +1,12 @@
+%#Template to generate a HTML table from a list of tuples (or a list of lists, or tuples of tuples...)
+<p>The open items are as follows:</p>
+<table border="1">
+%for row in rows:
+    <tr>
+    %for r in rows:
+        <td>{{r}}</td>
+    %end
+    </tr>
+%end
+</table>
+ 
